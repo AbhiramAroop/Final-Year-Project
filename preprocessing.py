8b4446dcb7064200a4f1a0488be66fceb766c89e
Abhiram Aroop <aaro0006@student.monash.edu>
2022-09-10 20:03:47 +1000

    Added full lists of data for preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,15 +3,16 @@ import os
 import Input_Output_file_processing as input_output_data
 
 mean_vals = []
+data_mean_input = []
 
-#computes the mean of a list
+# computes the mean of a list
 def compute_mean(num_list):
     if (len(num_list) != 0):
         return np.mean(num_list)
     return None
 
-def init_mean():
-    data = input_output_data.input_files_to_input_matrix(2)[1]
+#Initiallise the values for mean
+def init_mean(data):
     means = []
     mean_length = len(data[0][0])
     for i in range(mean_length):
@@ -24,16 +25,47 @@ def init_mean():
             if mean != None:
                 means[j].append(mean)
 
+
+
+
     return means
 
+#Computes the mean for all parametes
 def set_mean():
-    means = init_mean()
+    data = input_output_data.input_files_to_input_matrix(2)[1]
+    means = init_mean(data)
     for i in means:
         mean_vals.append(compute_mean(i))
 
     mean_vals[1] = round(mean_vals[1])
 
+    for i in range(len(data)):
+        data_mean_input.append([])
+
+    for i in range(len(data)):
+        values = data[i][1]
+        for j in range(len(values)):
+            mean = compute_mean(values[j])
+            if mean != None:
+                data_mean_input[i].append(mean)
+            else:
+                data_mean_input[j].append(mean_vals[j])
+
+def get_output_list():
+    data = input_output_data.read_output_file()
+    output = []
+    for i in data:
+        output.append(data[1])
+
+    return output
+#Get mean array
+def get_mean_list():
+    return mean_vals
+
+def get_mean_data():
+    set_mean()
+    return data_mean_input
 
 
-set_mean()
-print(mean_vals)
+#print(mean_vals)
+#print(data_mean_input)
